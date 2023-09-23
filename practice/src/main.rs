e5e10604a225004eb2b68a39b5a624f14b08d21a
shubham <shubham@saasden.club>
2023-09-23 14:18:10 +0000

    practice -2

--- practice/src/main.rs
+++ practice/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    // let  x=7;
+    let mut x=8;
+     println!("the value of x is {x}");
+    //  x=9;    // ERR=> cannot mutate immutable variable x
+    x=10;
+     println!("value of x =>{x}")
+
 }
