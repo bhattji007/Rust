0938fff4cfaeafd15d95064f2d17825a0bd03649
shubham <shubham@saasden.club>
2023-09-23 13:33:33 +0000

    Guessing_game-2

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,30 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 fn main()
 {
     println!("Guess a number");
     let secret_number=rand::thread_rng().gen_range(1..=100);
-    println!("The Secret number is {secret_number}");
+    // println!("The Secret number is {secret_number}");
+    loop{
     println!("Please Enter your guess");
     let mut guess = String::new();
     io::stdin()
     .read_line(&mut guess)
     .expect("Error Reading the line ");
+    let guess: u32 = match  guess.trim().parse(){
+        Ok(num)=>num ,
+        Err(_)=> continue,
+    };
     println!("You guessed: {guess}");
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Smaller oops!!"),
+        Ordering::Greater => println!("Greater oops!!"),
+        Ordering::Equal =>{
+             println!("Exactly the number!!");
+             break;
+            }
+    }
+
+}
 }
