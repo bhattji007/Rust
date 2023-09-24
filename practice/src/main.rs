a7e82c80fdcfb962eb54f9f3908673ba74115b47
shubham <shubham@saasden.club>
2023-09-24 13:45:58 +0000

    practice-3

--- practice/src/main.rs
+++ practice/src/main.rs
@@ -13,6 +13,7 @@ fn main() {
 
 
 //  Shadowing 
+ /*
  fn main(){
     let x=5;
     let x=x+1;
@@ -21,5 +22,30 @@ fn main() {
         println!("value of x in brackets {x}");  //8
     }
     println!("value of x outside brackets {x}");   //6
+
+ }
+ */
+
+
+//  Tuples
+/*fn main(){
+    let _tup: (i32, f64, u8) = (500, 6.4, 1);    // best way differentiating types
+    // destructuring 
+    let tup1=(500,2.1,1);
+    let (x,y,z)=tup1;
+    println!("The destructured values {x}, {y}, {z}");
+
+    // another way of assigning variables
+    let first=tup1.0;
+    let second=tup1.1;
+    let third = tup1.2;
+    println!("The values assigned are {first}, {second}, {third}");
+}
+
+ */
+
+//  Array 
+
+ fn main(){
 
  }
