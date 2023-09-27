417f581fccacc70a1c83de54ddcf61fdd2c9ada9
shubham <shubham@saasden.club>
2023-09-27 18:28:46 +0000

    day-4

--- practice/src/main.rs
+++ practice/src/main.rs
@@ -147,3 +147,5 @@ fn main() {
 
     println!("The result is {result}");
 }
+
+// Before the loop, we declare a variable named counter and initialize it to 0. Then we declare a variable named result to hold the value returned from the loop. On every iteration of the loop, we add 1 to the counter variable, and then check whether the counter is equal to 10. When it is, we use the break keyword with the value counter * 2. After the loop, we use a semicolon to end the statement that assigns the value to result. Finally, we print the value in result, which in this case is 20.
