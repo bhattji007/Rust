4426984a49ea676e50cc0d1642c907e9568f9549
shubham <shubham@saasden.club>
2023-09-27 18:29:14 +0000

    day-4

--- practice/src/main.rs
+++ practice/src/main.rs
@@ -134,18 +134,41 @@ fn another_function(x:i32,y:&str){
 
 // }
 
-fn main() {
-    let mut counter = 0;
+// fn main() {
+//     let mut counter = 0;
 
-    let result = loop {
-        counter += 1;
+//     let result = loop {
+//         counter += 1;
 
-        if counter == 10 {
-            break counter * 2;
-        }
-    };
+//         if counter == 10 {
+//             break counter * 2;
+//         }
+// //     };
 
-    println!("The result is {result}");
-}
+//     println!("The result is {result}");
+// }
 
 // Before the loop, we declare a variable named counter and initialize it to 0. Then we declare a variable named result to hold the value returned from the loop. On every iteration of the loop, we add 1 to the counter variable, and then check whether the counter is equal to 10. When it is, we use the break keyword with the value counter * 2. After the loop, we use a semicolon to end the statement that assigns the value to result. Finally, we print the value in result, which in this case is 20.
+
+
+fn main() {
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+}
